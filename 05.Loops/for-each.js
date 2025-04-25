bd9ade3575176a29985b5aecc011643256e8819b
Alen Chacko <the.alenchacko@gmail.com>
2025-04-25 17:39:18 +0530

    dynamic js

--- 05.Loops/for-each.js
+++ 05.Loops/for-each.js
@@ -2,18 +2,16 @@
 
 let numbers = [1, 2, 3, 4, 5];
 
-numbers.forEach(function(number) {
+numbers.forEach(function (number) {
   console.log(number);
 });
-
 
 // Example: Using forEach with Index
 let colors = ["red", "green", "blue"];
 
-colors.forEach(function(color, index) {
+colors.forEach(function (color, index) {
   console.log(index + ": " + color);
 });
-
 
 // Example: forEach with Arrow Functions
 let fruits = ["apple", "banana", "cherry"];
---
+++ 06.DynamicJS/script.js
@@ -0,0 +1,13 @@
+/*****************************************/
+// Changing Variable Types
+
+let value = 42; // Number
+console.log(typeof value); // "number"
+
+value = "Hello"; // Now it's a String
+console.log(typeof value); // "string"
+
+value = true; // Now it's a Boolean
+console.log(typeof value); // "boolean"
+
+/*****************************************/
