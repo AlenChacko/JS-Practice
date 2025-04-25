56aeeee562cd1e221a7f2c9f32eeb09a23cb5cfe
Alen Chacko <the.alenchacko@gmail.com>
2025-04-25 09:44:15 +0530

    Learned variable naming rules

---
+++ variables/rules.js
@@ -0,0 +1,18 @@
+// 3. Variable Naming Rules
+
+//Must begin with a letter, _, or $
+let company = 'Techno'
+let _name = 'Alen';
+let $age = 26;
+
+//Cannot start with a number
+// let 2name = 'Albin'
+
+//Case-sensitive (name ≠ Name)
+let student = 'Alen'; //
+let Student = 'Amal';//   Both are different variables
+console.log(`${student}\n${Student}`)
+
+//Cannot use reserved keywords like let, var, function, etc.
+
+// let function = 'test' 
