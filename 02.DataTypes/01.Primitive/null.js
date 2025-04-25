c51df1eef16f4b835e13d48336dc0362f776e8ba
Alen Chacko <the.alenchacko@gmail.com>
2025-04-25 11:49:19 +0530

    completed primitive data types and formatted code

--- 02.DataTypes/01.Primitive/null.js
+++ 02.DataTypes/01.Primitive/null.js
@@ -1,9 +1,7 @@
 // In JavaScript, null is a special value that means: "No value", or "deliberate absence of a value"
 
-
 let user = null; // user intentionally has no data yet
 typeof null; // ❗️ "object"
-
 
 let selectedUser = null;
 
