14b733a5faad18fef7d0a2169ee23ca924b7d7f0
Alen Chacko <the.alenchacko@gmail.com>
2025-04-25 20:51:19 +0530

    set object

---
+++ 13.Math/script.js
@@ -0,0 +1,38 @@
+// The Math object in JavaScript provides static properties and methods for mathematical constants and functions.
+
+// Math.abs(x) – Absolute value
+Math.abs(-5);  // 5
+Math.abs(5);   // 5
+
+// Math.ceil(x) – Rounds up
+Math.ceil(4.1);  // 5
+Math.ceil(-4.1); // -4
+
+// Math.floor(x) – Rounds down
+Math.floor(4.9);   // 4
+Math.floor(-4.1);  // -5
+
+// Math.round(x) – Rounds to nearest
+Math.round(4.5);   // 5
+Math.round(4.4);   // 4
+
+// Math.trunc(x) – Removes decimal part
+Math.trunc(4.9);   // 4
+Math.trunc(-4.9);  // -4
+
+// Math.pow(base, exponent) – Exponentiation
+Math.pow(2, 3);  // 8
+// Same as: 2 ** 3
+
+// Math.max(...values) – Highest value
+Math.max(3, 9, 5);     // 9
+Math.max(-1, -9, -5);  // -1
+
+// Math.min(...values) – Lowest value
+Math.min(3, 9, 5);     // 3
+Math.min(-1, -9, -5);  // -9
+
+//  Math.random() – Random number (between 0 and 1)
+Math.random();  // e.g., 0.7312
+
+
