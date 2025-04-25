e4947fbf9117f48955d0c9ed8276287cece20584
Alen Chacko <the.alenchacko@gmail.com>
2025-04-25 11:48:50 +0530

    completed null

---
+++ 02.DataTypes/01.Primitive/null.js
@@ -0,0 +1,11 @@
+// In JavaScript, null is a special value that means: "No value", or "deliberate absence of a value"
+
+
+let user = null; // user intentionally has no data yet
+typeof null; // ❗️ "object"
+
+
+let selectedUser = null;
+
+// Later...
+selectedUser = { name: "Alice" };
