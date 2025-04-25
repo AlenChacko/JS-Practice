849058f8c1983b3c7be011f33f18abacf944b148
Alen Chacko <the.alenchacko@gmail.com>
2025-04-25 17:40:29 +0530

    expressions vs statement

---
+++ 07.Expression-Statement/expressions.js
@@ -0,0 +1,2 @@
+5 + 10; // Expression that evaluates to 15
+"Hello" + " World"; // Expression that evaluates to "Hello World"
---
+++ 07.Expression-Statement/statement.js
@@ -0,0 +1,2 @@
+let x = 10; // Variable declaration statement
+if (x > 5) { console.log("x is greater than 5"); } // If statement
